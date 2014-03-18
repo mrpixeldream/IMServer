2f45810ac2b50f28a99a0329bf488e9951128399
mrpixeldream <carpetsoftware@yahoo.com>
2014-03-18 21:33:06 +0100

    Add timestamp to the messaging protocol

--- src/net/dreamcode/im/server/io/Message.java
+++ src/net/dreamcode/im/server/io/Message.java
@@ -1,16 +1,19 @@
 package net.dreamcode.im.server.io;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Message implements Serializable
 {
 	private static final long	serialVersionUID	= 1L;
 	
 	String message;
+	Date timestamp;
 	
 	public Message(String message)
 	{
 		this.message = message;
+		this.timestamp = new Date(System.currentTimeMillis());
 	}
 	
 	public void setMessage(String message)
